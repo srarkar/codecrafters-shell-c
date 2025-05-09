bec6af0e5f5de6829663e6c8376157aedf2ff67e
srarkar <rickdsarkar@gmail.com>
2025-05-09 18:15:38 -0400

    echo

--- src/main.c
+++ src/main.c
@@ -12,11 +12,23 @@ int main(int argc, char *argv[]) {
   // Wait for user input;
   char input[100];
   while (1) {
+    // take input and format by removing trailing new line
     fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0'; // set null terminator to remove trailing new line 
-    if (!strcmp(input, "exit 0")) {
+    input[strlen(input) - 1] = '\0';
+
+    // check for exit prompt
+    if (!strstr(input, "exit 0")) {
       break;
     }
+    
+    // check if echo is the first thing in input
+    char *echo_check = strstr(input, "echo");
+    if (echo_check == input) {
+      printf("%s", input[4]);
+      printf("$ ");
+      continue;
+    }
+
     printf("%s: command not found\n", input);
     printf("$ ");
   }
