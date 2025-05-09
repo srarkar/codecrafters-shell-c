e6674855b376bb18ed7af4c3e4781271a4dfe9c8
srarkar <rickdsarkar@gmail.com>
2025-05-09 09:50:05 -0400

    uncommented printf

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
+  Uncomment this block to pass the first stage
+  printf("$ ");
 
   // Wait for user input
   char input[100];
