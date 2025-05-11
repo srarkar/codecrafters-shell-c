8c2496a7c5b507656c5638b4830b4d82cb443a0e
srarkar <rickdsarkar@gmail.com>
2025-05-11 11:22:10 -0400

    step 7 finished

--- src/main.c
+++ src/main.c
@@ -20,20 +20,25 @@ static void type_handler(char* input, char** paths, int path_count) {
   char *next_token = strtok(input, " "); // grab token after "type"
   if (strstr(builtins, next_token) != NULL) {
     printf("%s is a shell builtin\n", next_token);
+    found = 1;
   } else {
+
     // search for next_token in PATH
     for (int i = 0; i < path_count; i++) {
       dir = opendir(paths[i]);
       while ((entry = readdir(dir)) != NULL) {
         if (strcmp(entry->d_name, next_token) == 0) {
-          printf("%s is %s/%s", next_token, paths[i], next_token);
+          printf("%s is %s/%s\n", next_token, paths[i], next_token);
           found = 1;
-          closedir(dir);
           break;
         }
       }
       closedir(dir);
+      if (found) {
+        break;
+      }
     }
+
   }
   if (!found) {
     printf("%s: not found\n", next_token);
@@ -53,8 +58,10 @@ int main(int argc, char *argv[]) {
   
   // grab PATH using getenv(). Alternatively, use parameter "char *envp[]" for main().
   char* path = strdup(getenv("PATH"));
-  // save original pointer for later free
-  char* path_copy = path; 
+  if (!path) {
+    printf("No PATH");
+    return 0;
+  }
 
    // allocate array of strings
   char** paths  = calloc((1000), sizeof(char*)); // not accepting a PATH with any more than 999 paths
@@ -66,20 +73,9 @@ int main(int argc, char *argv[]) {
   // separate PATH into tokens and store into string array 
   char *token;
   int path_count = 0;
-  char  delimiter = ':';
-  while ((token = strsep(&path, &delimiter)) != NULL) {
-    paths[path_count] = strdup(token); 
 
-    if (!paths[path_count]) {
-      printf("Path allocation: Out of memory");
-      for (int j = 0; j < path_count; j++) {
-        free(paths[j]); // free all previous elements if strdup fails
-      }
-      free(path_copy);
-      free(paths);
-      return 0;
-    }
-
+  while ((token = strsep(&path, ":")) != NULL) {
+    paths[path_count] = token; 
     path_count++;
   }
 
@@ -93,11 +89,6 @@ int main(int argc, char *argv[]) {
 
     // check for exit prompt
     if (!strcmp(input, "exit 0")) {
-      free(path_copy);
-      for (int j = 0; j < path_count; j++) {
-          free(paths[j]);
-      }
-      free(paths);
       break;
     }
 
